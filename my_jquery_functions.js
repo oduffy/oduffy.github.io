037f81fcf1668f686d54806801b8167887824c6d
ioduffy <isabellaoduffy@Isabellas-MacBook-Pro-2.local>
2016-08-16 19:43:38 +0100

    Drafts

---
+++ my_jquery_functions.js
@@ -0,0 +1,25 @@
+$(function() {
+
+$('.dropdown-toggle').click(function(){
+ $(this).next('.dropdown-menu').toggle();
+});
+
+$(document).click(function(e) {
+ var target = e.target;
+ if (!$(target).is('.dropdown-toggle') && !$(target).parents().is('.dropdown-toggle')) {
+   $('.dropdown-menu').hide();
+ }
+});
+
+});
+
+ 
+function initMap() {
+  var mapDiv = document.getElementById('map');
+  var map = new google.maps.Map(mapDiv, {
+      center: {lat:51.500152, lng: -0.126236},
+      zoom: 9
+  });
+}
+ 
+
