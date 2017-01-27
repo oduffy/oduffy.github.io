a34e9dbad66f82cd6b26fe3b9ea4eb031468577c
ioduffy <isabellaoduffy@Isabellas-MacBook-Pro-2.local>
2017-01-27 15:13:21 +0000

    Try again

--- Static/my_jquery_functions.js
+++ Static/my_jquery_functions.js
@@ -2,3 +2,11 @@ $('.carousel').carousel({
   interval: 6000,
   pause: "false"
 });
+
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $('nav').addClass('shrink');
+  } else {
+    $('nav').removeClass('shrink');
+  }
+});
